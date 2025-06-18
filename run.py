63ab56bdbe974299ed8e6c286e1a8da360e92bb3
DeividB-X <deividbertrand05@gmail.com>
2025-06-18 16:18:43 -0600

    Versión completa actualizada del proyecto

--- run.py
+++ run.py
@@ -4,5 +4,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+    print("SECRET_KEY:", os.getenv("SECRET_KEY") is not None)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
